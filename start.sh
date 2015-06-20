3b82cd583d2c698da8481a82de4ffc319d59d03c
FlareMars <a773207723@qq.com>
2015-06-21 03:05:20 +0800

    first commit

---
+++ start.sh
@@ -0,0 +1,6 @@
+export LC_APP_ID="kaxu5q5v5se28yovk2gx66eko4otjt4mfomrmjc7gfawvga4"
+export LC_APP_KEY="6urqrjr2kz6zyv6tgnra4v39sywt1kezawnheodjgu4h1fbh"
+export LC_APP_MASTER_KEY="2g5e8pb5gjv3uu34yck5xd7m8n6xg0oagpfgjgkaq2gmi4g9"
+export LC_APP_PORT=3000
+
+npm start
